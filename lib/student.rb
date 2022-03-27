78f6ad0b6ffed38db5a2d9ba6f08eb07fc40e253
katherineyuneman <kyuneman@gmail.com>
2022-03-27 13:31:40 -0400

    Test issues

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require 'pry'
+require_relative './user'
 
+class Student < User
+    # def initialize(knowledge)
+    #     @knowledge = []
+    # end
+
+    def learn(string_knowledge)
+        @knowledge << string_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
