48247b60c1ed321a4b72d4c20f2c6a32699b6a64
katherineyuneman <kyuneman@gmail.com>
2022-03-27 14:31:55 -0400

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,9 @@ require 'pry'
 require_relative './user'
 
 class Student < User
-    # def initialize(knowledge)
-    #     @knowledge = []
-    # end
+    def initialize
+        @knowledge = []
+    end
 
     def learn(string_knowledge)
         @knowledge << string_knowledge
--- lib/user.rb
+++ lib/user.rb
@@ -1,18 +1,18 @@
 class User
 
-    # attr_accessor :first_name, :last_name
+    attr_accessor :first_name, :last_name
 
-    def first_name=(first_name)
-        @first_name = first_name
-    end
+    # def first_name=(first_name)
+    #     @first_name = first_name
+    # end
 
-    def first_name
-        @first_name
-    end
+    # def first_name
+    #     @first_name
+    # end
 
-    def initialize(first_name, last_name)
-        @first_name = first_name
-        @last_name = last_name
-    end
+    # def initialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    # end
 
 end
